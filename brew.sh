5773135ed668e412e1586f4a7dbf21c95a173d2d
Max Bo <max@maxbo.me>
2016-01-17 21:49:04 +1000

    Add brew script

---
+++ brew.sh
@@ -0,0 +1,19 @@
+brew update
+brew upgrade
+brew tap caskroom/versions
+
+brew install python3
+brew install maven
+brew cask install java
+brew cask install dropbox
+brew cask install google-chrome
+brew cask install gimp
+brew cask install vlc
+brew cask install sourcetree
+brew cask install iterm2
+brew install screenfetch
+brew install ranger
+brew install the_silver_searcher
+brew install weechat
+brew install brew install macvim --with-override-system-vim
+brew install zsh
